3a4fe69d847b991228e549ad820aa0a2e6626c57
Soar Qin <soarchin@gmail.com>
2024-10-07 00:18:07 +0800

    WIP

---
+++ filters/2hgin3bg.cc
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2023 Soar Qin<soarchin@gmail.com>
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+#include "filter.hh"
+
+extern "C" {
+
+__declspec(dllexport) const char *FILTERAPI init(PluginAPI*, int *type) {
+    *type = 0;
+    return "2 Blue Giants with high luminosity in 3 Blue Giant Seeds";
+}
+
+__declspec(dllexport) bool FILTERAPI galaxyFilter(const dspugen::Galaxy *g) {
+    int cnt2 = 0;
+    for (const auto *star: g->stars) {
+        if (star->type == dspugen::EStarType::GiantStar && star->spectr == dspugen::ESpectrType::O) {
+            if (star->luminosity >= /*19.832529646959319302266016012115f*/ 18.092348467648446913917646190829f /*16.064927362833999424416458640845f*/) {
+                ++cnt2;
+            }
+        }
+    }
+    if (cnt2 >= 2) {
+        fprintf(stdout, "%d,%d\n", g->seed, g->starCount);
+        return true;
+    }
+    return false;
+}
+
+}
--- filters/dpoutput.cc
+++ filters/dpoutput.cc
@@ -22,12 +22,39 @@ __declspec(dllexport) const char *FILTERAPI init2(PluginAPI *api, int *type, boo
     planets = hasPlanets;
     starOut = std::ofstream("dp_stars.csv");
     fmt::print(starOut,
-               "种子,星系数,母星巨星类型,母星地表可燃冰,母星巨星卫星数,蓝巨1亮度,蓝巨1行星数,蓝巨2亮度,蓝巨2行星数,O星数,水世界数,橙晶数,磁石簇数\n");
+               "种子,星系数,母星巨星类型,母星地表可燃冰,母星巨星卫星数,蓝巨1亮度,蓝巨1行星数,蓝巨2亮度,蓝巨2行星数,O星数,水世界数,橙晶数,高产气巨数,油井数,磁石簇数,全包行星数\n");
     return "DSP-Power Related Output";
 }
 
 __declspec(dllexport) void FILTERAPI uninit() {
     starOut.close();
+}
+
+
+inline bool isThemeFullPower(int theme, double orbitRadius, double dysonRadius) {
+    switch (theme) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 21:
+            return false;
+        case 11:
+            return orbitRadius <= dysonRadius * 0.738461538;
+        case 7:
+            return orbitRadius <= dysonRadius * 1.357047245;
+        case 10:
+        case 20:
+        case 23:
+        case 24:
+            return orbitRadius <= dysonRadius * 1.402768768;
+        case 6:
+        case 13:
+        case 19:
+            return orbitRadius <= dysonRadius * 1.537522017;
+        default:
+            return orbitRadius <= dysonRadius * 1.448041666;
+    }
 }
 
 __declspec(dllexport) void FILTERAPI output(const dspugen::Galaxy *galaxy) {
@@ -42,8 +69,17 @@ __declspec(dllexport) void FILTERAPI output(const dspugen::Galaxy *galaxy) {
     int waterCount = 0;
     int orangeCount = 0;
     int magnetCount = 0;
+    int steeps = 0;
+
+    int lumId[64];
+    int lumCnt = 0;
+    int hgId[64];
+    int hgCnt = 0;
     if (!planets) theAPI->GenerateAllPlanets(galaxy);
     for (auto *star: galaxy->stars) {
+        for (const auto *planet: star->planets) {
+            steeps += planet->veinSpot[7];
+        }
         if (star->index == 0) {
             for (const auto *planet: star->planets) {
                 switch (planet->theme) {
@@ -77,8 +113,19 @@ __declspec(dllexport) void FILTERAPI output(const dspugen::Galaxy *galaxy) {
                                 case 3:
                                 case 4:
                                 case 5:
-                                case 21:
                                     break;
+                                case 21: {
+                                    theAPI->GeneratePlanetGas(planet);
+                                    auto &gasItems = planet->gasItems;
+                                    size_t sz = gasItems.size();
+                                    for (size_t i = 0; i < sz; i++) {
+                                        /* Gas Giant with Deuterium(1121) >= 0.15 (0.1875x0.8 under 0.1x resource) */
+                                        if (gasItems[i] == 1121 && planet->gasSpeeds[i] >= 0.1875f) {
+                                            hgId[hgCnt++] = planet->id;
+                                        }
+                                    }
+                                    break;
+                                }
                                 default:
                                     cnt++;
                                     break;
@@ -97,6 +144,20 @@ __declspec(dllexport) void FILTERAPI output(const dspugen::Galaxy *galaxy) {
                     if (star->spectr == dspugen::ESpectrType::O) {
                         oCount++;
                     }
+                    if (star->luminosity >= 4.99263753f) {
+                        auto *planet = star->planets[0];
+                        auto dysonRad = std::round(star->dysonRadius * 40000.0 / 100.0) * 100.0;
+                        if (isThemeFullPower(planet->theme, planet->orbitRadius * 40000.0, dysonRad)) {
+                            lumId[lumCnt++] = planet->id;
+                        }
+                        /* Luminosity >= 2.04f, can have 2 full planet photon receivers */
+                        if (star->luminosity >= 8.675074184f) {
+                            planet = star->planets[1];
+                            if (isThemeFullPower(planet->theme, planet->orbitRadius * 40000.0, dysonRad)) {
+                                lumId[lumCnt++] = planet->id;
+                            }
+                        }
+                    }
                     for (const auto *planet: star->planets) {
                         switch (planet->theme) {
                             case 16:
@@ -105,6 +166,18 @@ __declspec(dllexport) void FILTERAPI output(const dspugen::Galaxy *galaxy) {
                             case 23:
                                 orangeCount++;
                                 break;
+                            case 21: {
+                                theAPI->GeneratePlanetGas(planet);
+                                auto &gasItems = planet->gasItems;
+                                size_t sz = gasItems.size();
+                                for (size_t i = 0; i < sz; i++) {
+                                    /* Gas Giant with Deuterium(1121) >= 0.15 (0.1875x0.8 under 0.1x resource) */
+                                    if (gasItems[i] == 1121 && planet->gasSpeeds[i] >= 0.1875f) {
+                                        hgId[hgCnt++] = planet->id;
+                                    }
+                                }
+                                break;
+                            }
                             default:
                                 break;
                         }
@@ -121,7 +194,7 @@ __declspec(dllexport) void FILTERAPI output(const dspugen::Galaxy *galaxy) {
             }
         }
     }
-    fmt::print(starOut, "{},{},{},{},{},{:.3f},{},{:.3f},{},{},{},{},{}\n",
+    fmt::print(starOut, "{},{},{},{},{},{:.3f},{},{:.3f},{},{},{},{},{},{},{},{}\n",
                galaxy->seed,
                galaxy->starCount,
                isGas ? "气" : "冰",
@@ -134,7 +207,10 @@ __declspec(dllexport) void FILTERAPI output(const dspugen::Galaxy *galaxy) {
                oCount,
                waterCount,
                orangeCount,
-               magnetCount);
+               hgCnt,
+               steeps,
+               magnetCount,
+               lumCnt);
 }
 
 }
